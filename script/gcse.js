438cf347e035dd04d44d1be212689ea93d967a85
Werreales <werreales.ns@gmail.com>
2018-01-04 20:44:38 +0100

    Skeleton of navi

---
+++ script/gcse.js
@@ -0,0 +1,11 @@
+(function() {
+	const cx = '000495284481177784377:u39rq_c8v1e';
+	const gcse = document.createElement('script');
+
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+
+	const s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+})();
